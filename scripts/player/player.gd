ae6f54c18c4b9fb7adf4f88a158b0a38df90fde8
Anderson <llima.anderson26@gmail.com>
2026-08-01 22:30:47 -0300

    v0.0.4 Sistema de chute

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 const SPEED := 6.0
+const KICK_FORCE := 20.0
+@onready var ball = $"../Ball"
 
 func _physics_process(delta):
 	var direction := Vector3.ZERO
@@ -19,7 +21,17 @@ func _physics_process(delta):
 
 	direction = direction.normalized()
 
+	if direction != Vector3.ZERO:
+		rotation.y = atan2(direction.x, direction.z)
+
 	velocity.x = direction.x * SPEED
 	velocity.z = direction.z * SPEED
 
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("kick"):
+		var distance = global_position.distance_to(ball.global_position)
+
+		if distance < 2.0:
+			var dir = (ball.global_position - global_position).normalized()
+			ball.apply_central_impulse(dir * KICK_FORCE)
