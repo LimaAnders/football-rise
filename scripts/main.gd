4624b63cb7753fd9dc74357f2e4b5824ab92ab64
Anderson <llima.anderson26@gmail.com>
2026-08-04 19:18:00 -0300

    v0.0.7 Placar funcional

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,27 +2,32 @@ extends Node3D
 
 @onready var ball = $Ball
 @onready var player = $Player
-
 @onready var ball_spawn = $BallSpawn
 @onready var player_spawn = $PlayerSpawn
+@onready var score_label = $UI/ScoreLabel
+
+var score_top := 0
+var score_bottom := 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func _ready():
+	update_score()# Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_goal_top_body_entered(body):
 	if body.name == "Ball":
+		score_bottom += 1
+		update_score()
 		print("GOOOOL! Time de baixo marcou!")
 		reset_match()
 
 func _on_goal_bottom_body_entered(body):
 	if body.name == "Ball":
+		score_top += 1
+		update_score()
 		print("GOOOOL! Time de cima marcou!")
 		reset_match()
 	
@@ -35,3 +40,6 @@ func reset_match():
 	# Reposiciona o jogador
 	player.velocity = Vector3.ZERO
 	player.global_position = player_spawn.global_position
+
+func update_score():
+	score_label.text = str(score_top) + " x " + str(score_bottom)
