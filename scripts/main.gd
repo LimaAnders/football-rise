f377c8e503f22bed9cb0a8fd33d230cbbce25ed2
Anderson <llima.anderson26@gmail.com>
2026-08-04 19:05:00 -0300

    v0.0.6 Sistema de gol funcional

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,10 @@
 extends Node3D
 
+@onready var ball = $Ball
+@onready var player = $Player
+
+@onready var ball_spawn = $BallSpawn
+@onready var player_spawn = $PlayerSpawn
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +17,21 @@ func _process(delta: float) -> void:
 
 
 func _on_goal_top_body_entered(body):
-	print("Entrou:", body.name)
+	if body.name == "Ball":
+		print("GOOOOL! Time de baixo marcou!")
+		reset_match()
+
+func _on_goal_bottom_body_entered(body):
+	if body.name == "Ball":
+		print("GOOOOL! Time de cima marcou!")
+		reset_match()
+	
+func reset_match():
+	# Reposiciona a bola
+	ball.linear_velocity = Vector3.ZERO
+	ball.angular_velocity = Vector3.ZERO
+	ball.global_position = ball_spawn.global_position
+
+	# Reposiciona o jogador
+	player.velocity = Vector3.ZERO
+	player.global_position = player_spawn.global_position
