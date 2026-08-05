8baacb120078b6b4434f5b6194b5f8e542b4186a
Anderson <llima.anderson26@gmail.com>
2026-08-05 18:54:17 -0300

    v0.0.8 Cronômetro da Partida

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,9 +9,14 @@ extends Node3D
 var score_top := 0
 var score_bottom := 0
 
+@onready var timer_label = $UI/TimerLabel
+
+var match_time := 90
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update_score()# Replace with function body.
+	update_score()
+	update_timer()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -43,3 +48,18 @@ func reset_match():
 
 func update_score():
 	score_label.text = str(score_top) + " x " + str(score_bottom)
+
+func update_timer():
+	var minutes := int(match_time / 60)
+	var seconds := int(match_time % 60)
+
+	timer_label.text = "%02d:%02d" % [minutes, seconds]
+
+
+func _on_match_timer_timeout() -> void:
+	if match_time > 0:
+		match_time -= 1
+		update_timer()
+	else:
+		$MatchTimer.stop()
+		print("Fim da partida!")# Replace with function body.
