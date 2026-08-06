8fbe5184303961dca79d8c063a9ef3530018e386
Anderson <llima.anderson26@gmail.com>
2026-08-06 18:35:48 -0300

    v0.1.1 Sistema inicial de domínio da bola

--- scripts/ball/ball.gd
+++ scripts/ball/ball.gd
@@ -1,1 +1,33 @@
 extends RigidBody3D
+
+enum BallState {
+	FREE,
+	CONTROLLED
+}
+
+var state := BallState.FREE
+var ball_owner: CharacterBody3D = null
+var control_cooldown := 0.0
+
+const CONTROL_DISTANCE := 1.2
+const FOLLOW_SPEED := 8.0
+
+func _physics_process(delta):
+	if control_cooldown > 0.0:
+		control_cooldown -= delta
+
+	# Enquanto estiver no cooldown, não deixa a bola ser controlada
+	if control_cooldown > 0.0:
+		return
+
+	if state == BallState.CONTROLLED and ball_owner != null:
+
+		var target = ball_owner.get_node("BallControlPoint").global_position
+
+		var distance = global_position.distance_to(target)
+
+		if distance > 0.05:
+			global_position = global_position.move_toward(target, FOLLOW_SPEED * delta)
+
+		linear_velocity = Vector3.ZERO
+		angular_velocity = Vector3.ZERO
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 const SPEED := 6.0
 const KICK_FORCE := 20.0
 @onready var ball = $"../Ball"
+const BALL_CONTROL_DISTANCE := 2.0
+const DRIBBLE_FORCE := 6.0
 
 @export var move_forward_action := "move_forward"
 @export var move_back_action := "move_back"
@@ -36,8 +38,17 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	if Input.is_action_just_pressed(kick_action):
-		var distance = global_position.distance_to(ball.global_position)
+		if ball.ball_owner == self:
 
-		if distance < 2.0:
+			ball.state = ball.BallState.FREE
+			ball.ball_owner = null
+			ball.control_cooldown = 0.4
+
 			var dir = (ball.global_position - global_position).normalized()
 			ball.apply_central_impulse(dir * KICK_FORCE)
+		
+	if ball.control_cooldown <= 0.0:
+		if global_position.distance_to(ball.global_position) <= BALL_CONTROL_DISTANCE:
+			ball.ball_owner = self
+			ball.state = ball.BallState.CONTROLLED
+	
